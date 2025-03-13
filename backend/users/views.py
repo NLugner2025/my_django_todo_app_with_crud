8ef365e82f8380ad457d1bab925b1d32dcae197d
Nils Lugner <nils@lugner.uk>
2025-03-13 09:01:01 +0000

    Update views.py
    Updated.

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,9 +1,7 @@
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 
-
 from .forms import UserCreationForm
-
 class RegisterView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
